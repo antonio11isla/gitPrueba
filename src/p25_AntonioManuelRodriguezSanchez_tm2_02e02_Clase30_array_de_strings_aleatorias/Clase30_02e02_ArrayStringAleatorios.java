9c7faf91ef9a364ee60816289db5f358c6331c67
Antonio <antonio140101@gmail.com>
2020-05-15 13:29:42 +0200

    mi primer commit

---
+++ src/p25_AntonioManuelRodriguezSanchez_tm2_02e02_Clase30_array_de_strings_aleatorias/Clase30_02e02_ArrayStringAleatorios.java
@@ -0,0 +1,46 @@
+package p25_AntonioManuelRodriguezSanchez_tm2_02e02_Clase30_array_de_strings_aleatorias;
+
+import java.util.Arrays;
+
+public class Clase30_02e02_ArrayStringAleatorios {
+
+    public static void crearArray() {
+        //Se crean los números aleatorios que serán el tamaño de cada array
+        int numAleatorioS = (int) ((Math.random() * 6) + 2);
+        int numAleatorio = (int) ((Math.random() * 9) + 1);
+
+//Se crea la array de String aleatorios
+        String arrayString[] = new String[numAleatorioS];
+        System.out.println("Eco de array de String aleatorios:");
+//For para recorrer todos los elementos de la array de String aleatorios
+        for (int i = 0; i < arrayString.length; i++) {
+//Se crea arrays de caracteres que serán cada uno de los elementos aleatorios
+//de la array de String
+            char arrayCaracteres[] = new char[numAleatorio];
+//For que recorre todas las arrays de String y le da valores aleatorio
+//del código ASCII
+            for (int y = 0; y < arrayCaracteres.length; y++) {
+                int x = (int) (Math.random() * 255);
+                char c = (char) x;
+                arrayCaracteres[y] = c;
+            }
+//Se crean los String aleatorios a partir de las arrays de caracteres
+            String cadena = new String(arrayCaracteres);
+            arrayString[i] = cadena;
+//Se saca por pantalla cada elemento de la array de String
+            System.out.print(arrayString[i]+ " ,");
+        }
+
+//Se ordena la array de String aleatorios
+        Arrays.sort(arrayString);
+//Se muestra por pantalla la array de String aleatorios ordenada
+        System.out.println("\b\n\nEco de array de String aleatorios ordenada:\n" + Arrays.toString(arrayString));
+    }
+
+    public static void main(String[] args) {
+
+        crearArray();
+        
+    }
+
+}
